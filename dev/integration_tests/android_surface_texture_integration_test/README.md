1d9ba6ca5dacc3ed803ab48eeca16cd0b9fcd85d
Matan Lurey <matan@lurey.org>
2024-01-22 16:27:54 -0800

    Rename external_ui to android_surface_texture_integration_test.

---
+++ dev/integration_tests/android_surface_texture_integration_test/README.md
@@ -0,0 +1,22 @@
+# android_surface_texture_integration_test
+
+A Flutter project for testing external texture rendering on Android.
+
+## Contributing
+
+Unfortunately [`flutter_driver` is considered deprecated][1] and
+[no documentation exists](https://github.com/flutter/flutter/issues/142021), so
+just do your best.
+
+[1]: https://docs.flutter.dev/release/breaking-changes/flutter-driver-migration
+
+## Running
+
+Connect an Android device or launch an emulator. Then run:
+
+```shell
+# Assuming you are in the root of the flutter repo.
+
+$ cd dev/integration_tests/android_surface_texture_integration_test
+$ flutter drive
+```
--- dev/integration_tests/android_surface_texture_integration_test/android/app/src/main/java/io/flutter/externalui/MainActivity.java
+++ dev/integration_tests/android_surface_texture_integration_test/android/app/src/main/java/io/flutter/externalui/MainActivity.java
@@ -13,11 +13,6 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.View;
 import androidx.annotation.NonNull;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.android.FlutterSurfaceView;
 import io.flutter.embedding.android.FlutterView;
@@ -26,9 +21,12 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.TextureRegistry;
 import io.flutter.view.TextureRegistry.SurfaceTextureEntry;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends FlutterActivity {
   private Surface surface;
@@ -43,86 +41,89 @@ public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
-    final MethodChannel channel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "texture");
+    final MethodChannel channel = new MethodChannel(
+        flutterEngine.getDartExecutor().getBinaryMessenger(), "texture");
     channel.setMethodCallHandler(new MethodCallHandler() {
       @Override
       public void onMethodCall(MethodCall methodCall, Result result) {
         switch (methodCall.method) {
-          case "start":
-            framesProduced.set(0);
-            framesConsumed.set(0);
-            final int fps = methodCall.arguments();
-            final FrameRenderer renderer = new FrameRenderer(surface);
-            producerTimer = new Timer();
-            producerTimer.scheduleAtFixedRate(new TimerTask() {
-              @Override
-              public void run() {
-                final long time = System.currentTimeMillis();
-                if (frameRate(framesProduced, startTime, time) < fps) {
-                  renderer.drawFrame();
-                  framesProduced.incrementAndGet();
-                }
+        case "start":
+          framesProduced.set(0);
+          framesConsumed.set(0);
+          final int fps = methodCall.arguments();
+          final FrameRenderer renderer = new FrameRenderer(surface);
+          producerTimer = new Timer();
+          producerTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+              final long time = System.currentTimeMillis();
+              if (frameRate(framesProduced, startTime, time) < fps) {
+                renderer.drawFrame();
+                framesProduced.incrementAndGet();
               }
-            }, 0, 1000 / fps);
-            consumerTimer = new Timer();
-            consumerTimer.scheduleAtFixedRate(new TimerTask() {
-              private long lastTimestamp = -1L;
+            }
+          }, 0, 1000 / fps);
+          consumerTimer = new Timer();
+          consumerTimer.scheduleAtFixedRate(new TimerTask() {
+            private long lastTimestamp = -1L;
 
-              @Override
-              public void run() {
-                final long timestamp = texture.getTimestamp();
-                // The texture's timestamp is updated on consumption.
-                // We detect the change by asking very frequently.
-                if (timestamp != lastTimestamp) {
-                  lastTimestamp = timestamp;
-                  framesConsumed.incrementAndGet();
-                }
+            @Override
+            public void run() {
+              final long timestamp = texture.getTimestamp();
+              // The texture's timestamp is updated on consumption.
+              // We detect the change by asking very frequently.
+              if (timestamp != lastTimestamp) {
+                lastTimestamp = timestamp;
+                framesConsumed.incrementAndGet();
               }
-            }, 0, 1);
-            startTime = System.currentTimeMillis();
-            result.success(null);
-            break;
-          case "stop":
-            producerTimer.cancel();
-            consumerTimer.cancel();
-            endTime = System.currentTimeMillis();
-            result.success(null);
-            break;
-          case "getProducedFrameRate":
-            result.success(frameRate(framesProduced, startTime, endTime));
-            break;
-          case "getConsumedFrameRate":
-            result.success(frameRate(framesConsumed, startTime, endTime));
-            break;
-          default: result.notImplemented();
+            }
+          }, 0, 1);
+          startTime = System.currentTimeMillis();
+          result.success(null);
+          break;
+        case "stop":
+          producerTimer.cancel();
+          consumerTimer.cancel();
+          endTime = System.currentTimeMillis();
+          result.success(null);
+          break;
+        case "getProducedFrameRate":
+          result.success(frameRate(framesProduced, startTime, endTime));
+          break;
+        case "getConsumedFrameRate":
+          result.success(frameRate(framesConsumed, startTime, endTime));
+          break;
+        default:
+          result.notImplemented();
         }
       }
     });
   }
 
   @Override
-  public void onFlutterSurfaceViewCreated(@NonNull FlutterSurfaceView flutterSurfaceView) {
+  public void
+  onFlutterSurfaceViewCreated(@NonNull FlutterSurfaceView flutterSurfaceView) {
     flutterSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
       @Override
       public void surfaceCreated(SurfaceHolder holder) {
-        final SurfaceTextureEntry textureEntry = flutterSurfaceView.getAttachedRenderer().createSurfaceTexture();
+        final SurfaceTextureEntry textureEntry =
+            flutterSurfaceView.getAttachedRenderer().createSurfaceTexture();
         texture = textureEntry.surfaceTexture();
         texture.setDefaultBufferSize(300, 200);
         surface = new Surface(texture);
       }
 
       @Override
-      public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-      }
+      public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                 int height) {}
 
       @Override
-      public void surfaceDestroyed(SurfaceHolder holder) {
-      }
+      public void surfaceDestroyed(SurfaceHolder holder) {}
     });
   }
 
   double frameRate(AtomicInteger frameCounter, long startTime, long endTime) {
-    return frameCounter.get() * 1000 / (double) (endTime - startTime);
+    return frameCounter.get() * 1000 / (double)(endTime - startTime);
   }
 
   @Override
@@ -148,7 +149,7 @@ class FrameRenderer {
   }
 
   void drawFrame() {
-    final Canvas canvas = surface.lockCanvas(null);
+    final Canvas canvas = surface.lockHardwareCanvas();
     canvas.drawColor(0xff000000);
     canvas.drawText(String.valueOf(++frameCount), 20, 120, paint);
     surface.unlockCanvasAndPost(canvas);
